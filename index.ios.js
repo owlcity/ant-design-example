1b4df3ce54c36fd5f2d3750f14837b96db1284f1
owlcity <596718356@qq.com>
2017-10-10 18:23:53 +0800

    index.ios.js modify

--- index.ios.js
+++ index.ios.js
@@ -1,78 +1,81 @@
-import React from 'react';
-import {AppRegistry} from 'react-native';
-import { Toast, WhiteSpace, WingBlank, Button } from 'antd-mobile';
-import { DeviceEventEmitter } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Button, TabBar, Tabs} from 'antd-mobile';
 
-function showToast() {
-  Toast.info('This is a toast tips !!!');
-}
-
-function successToast() {
-  Toast.success('Load success !!!', 1);
-}
-
-function showToastNoMask() {
-  Toast.info('Toast without mask !!!', 1, null, false);
-}
-
-function failToast() {
-  Toast.fail('Load failed !!!');
-}
-
-function offline() {
-  Toast.offline('Network connection failed !!!');
-}
-
-function loadingToast() {
-  Toast.loading('Loading...', 1, () => {
-    console.log('Load complete !!!');
-  });
-}
-
-export default class ToastExample extends React.Component<any, any> {
-  timer: any;
-  componentDidMount() {
-    DeviceEventEmitter.addListener('navigatorBack', () => {
-      Toast.hide();
-    });
+export default class Test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'blueTab',
+    };
   }
-
-  componentWillUnmount () {
-    (DeviceEventEmitter).removeAllListeners('navigatorBack');
-    if (this.timer) {
-      clearTimeout(this.timer);
-      this.timer = null;
-    }
-  }
-
-  alwaysShowToast = () => {
-    Toast.info('A toast width duration = 0 !!!', 0);
-    this.timer = setTimeout(() => {
-      Toast.hide();
-    }, 5000);
+  onPress(selectedTab) {
+    this.setState({
+      selectedTab
+    })
   }
 
   render() {
     return (
-      <WingBlank style={{ marginTop: 80 }}>
-        <WhiteSpace />
-        <Button onClick={showToastNoMask}>Without mask</Button>
-        <WhiteSpace />
-        <Button onClick={showToast}>Text toast</Button>
-        <WhiteSpace />
-        <Button onClick={successToast}>Success toast</Button>
-        <WhiteSpace />
-        <Button onClick={failToast}>Failed toast</Button>
-        <WhiteSpace />
-        <Button onClick={offline}>Network failure toast</Button>
-        <WhiteSpace />
-        <Button onClick={loadingToast}>Loading toast</Button>
-        <WhiteSpace />
-        <Button onClick={this.alwaysShowToast}>Toast width duration = 0</Button>
-        <WhiteSpace />
-      </WingBlank>
+      <TabBar
+        unselectedTintColor="#949494"
+        tintColor="#33A3F4"
+        barTintColor="white"
+      >
+        <TabBar.Item
+          title="基础"
+          key="基础"
+          icon={require('./Components/img/alipay@3x.png')}
+          selectedIcon={require('./Components/img/alipay_sel@3x.png')}
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={this.onPress.bind(this, 'blueTab')}
+        >
+          <View style={styles.container}>
+            <Text>基础</Text>
+          </View>
+        </TabBar.Item>
+        <TabBar.Item
+          title="导航"
+          key="导航"
+          icon={require('./Components/img/koubei@3x.png')}
+          selectedIcon={require('./Components/img/koubei_sel@3x.png')}
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={this.onPress.bind(this, 'redTab')}
+        >
+          <View style={styles.container}>
+            <Text>导航</Text>
+          </View>
+        </TabBar.Item>
+        <TabBar.Item
+          title="展示"
+          key="展示"
+          icon={require('./Components/img/friend@3x.png')}
+          selectedIcon={require('./Components/img/friend_sel@3x.png')}
+          selected={this.state.selectedTab === 'greenTab'}
+          onPress={this.onPress.bind(this, 'greenTab')}
+        >
+          <View style={styles.container}><Text>展示</Text></View>
+        </TabBar.Item>
+        <TabBar.Item
+          title="其它"
+          key="其它"
+          icon={require('./Components/img/busi@3x.png')}
+          selectedIcon={require('./Components/img/busi_sel@3x.png')}
+          selected={this.state.selectedTab === 'yellowTab'}
+          onPress={this.onPress.bind(this, 'yellowTab')}
+        >
+          <View style={styles.container}><Text>其它</Text></View>
+        </TabBar.Item>
+      </TabBar>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#F5FCFF',
+  },
+});
 // change xxxxx to your project name
-AppRegistry.registerComponent('Ant', () => ToastExample);
+AppRegistry.registerComponent('Ant', () => Test);
